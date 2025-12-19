1e6fe040f125b21bdafbbeeecadac48b3daa269f
geekible-ltd <aws.root@geekible.org>
2025-12-19 11:16:55 +0000

    exposed methods for getting the db and router

--- service-framework.go
+++ service-framework.go
@@ -1,1 +1,34 @@
 package serviceframework
+
+import (
+	frameworkdto "github.com/geekible-ltd/serviceframework/framework-dto"
+	"github.com/geekible-ltd/serviceframework/internal/config"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ServiceFramework struct {
+	cfg    *frameworkdto.FrameworkConfig
+	fc     *config.FrameworkConfiguration
+	db     *gorm.DB
+	router *gin.Engine
+}
+
+func NewServiceFramework(cfg *frameworkdto.FrameworkConfig) *ServiceFramework {
+	fc := config.NewFrameworkConfig(cfg)
+	gormDb := fc.GetDatabase()
+
+	return &ServiceFramework{
+		cfg: cfg,
+		fc:  fc,
+		db:  gormDb,
+	}
+}
+
+func (s *ServiceFramework) GetDatabase() *gorm.DB {
+	return s.db
+}
+
+func (s *ServiceFramework) GetRouter() *gin.Engine {
+	return s.router
+}
